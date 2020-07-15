4100046fc8fc7881b05335b2f1416113000937fa
Mr.sun <18273965210@163.com>
2020-07-15 17:41:04 +0800

    fix

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -2,7 +2,7 @@ const assert = require('assert')
 describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js')
     it('entry', () => {
-        assert.equal(baseConfig.entry.index,'/Users/sunjianming/Desktop/github/webpack/mrsun_build_webpack/test/smoke/template/src/index/index.js')
-        assert.equal(baseConfig.entry.search,'/Users/sunjianming/Desktop/github/webpack/mrsun_build_webpack/test/smoke/template/src/search/index.js')
+        assert.equal(baseConfig.entry.index.indexOf('test/smoke/template/src/index/index.js'>-1),true)
+        assert.equal(baseConfig.entry.search.indexOf('test/smoke/template/src/search/index.js')>-1,true)
     })
 })
